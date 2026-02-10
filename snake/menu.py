24309615b2211cb6d7d5927ea145f4fe63b3f3f9
Green2115 <greentkaczyk2011@gmail.com>
2026-02-10 23:08:45 +0100

    Implement menu class for game initialization
    Added a menu class to handle game menu events and initialization.

--- snake/menu.py
+++ snake/menu.py
@@ -1,4 +1,27 @@
-import Main
-from Main import start_game
 
-start_game
+from Main import start_game
+from Main import Game
+import pygame
+from pygame import Vector2
+display_width = 800
+display_heigth = 600
+screen = pygame.display.set_mode((display_width, display_heigth))
+clock = pygame.time.Clock()
+pygame.init()
+
+class menu:
+    def __init__(self, running):
+        self.running = running
+
+m = menu(True)
+
+while m.running:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            m.running = False
+
+    
+
+    pygame.display.flip()
+    clock.tick(60)
